85cfd2f5937b4cada8fce038f7822509152cd501
ars1mm <arsimajvazi420@gmail.com>
2024-09-30 23:24:47 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Navbar from './components/Navbar'
 import Model from './components/Model'
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 const  App =() => {
   return (
@@ -12,6 +15,9 @@ const  App =() => {
       <Hero/>
       <Highlights/>
       <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
   )
 }
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,9 @@
 import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+
 import * as THREE from 'three'
 import Lights from './Lights';
 import Loader from './Loader';
+import IPhone from './Iphone';
 import { Suspense } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
@@ -30,7 +32,11 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
 
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
         <Suspense fallback={<Loader />}>
-          
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
         </Suspense>
       </group>
     </View>
